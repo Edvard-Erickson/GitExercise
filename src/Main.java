b6a1aabdaab8e87cc44821eb90e5b2d63c4b007f
Edvard-Erickson <ericksoneg22@gcc.edu>
2025-02-17 14:16:32 -0500

    added loop to main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("My name is Bob");
+
+        for(int i = 0 ; i < 10; i++){
+            System.out.print(i + " ");
+        }
     }
 }
